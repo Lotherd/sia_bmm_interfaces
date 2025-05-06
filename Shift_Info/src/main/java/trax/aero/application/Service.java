a6d8dd5fb78fda1fe220466b11240c46f63e4774
Ernesto Rodriguez <ernestorodriguez374@gmail.com>
2025-05-06 12:48:08 +0800

    418014/ https://traxcorp.atlassian.net/browse/INTERFACE-2218 Start Development

---
+++ Shift_Info/src/main/java/trax/aero/application/Service.java
@@ -0,0 +1,35 @@
+package trax.aero.application;
+
+
+
+import java.util.logging.Logger;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+import trax.aero.logger.LogManager;
+
+
+
+@Path("/ShiftInfoService")
+public class Service {
+	
+	EntityManagerFactory factory;
+	Logger logger = LogManager.getLogger("ShiftInfo_I01");
+	
+	@GET
+    @Path("/healthCheck")
+    @Produces(MediaType.APPLICATION_XML + ";charset=UTF-8" )
+    public Response healthCheck() 
+    {    	
+		logger.info("Healthy");
+    	return Response.ok("Healthy",MediaType.APPLICATION_XML).build();
+    }
+}
