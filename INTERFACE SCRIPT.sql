a7f8309f89441a47e4098590b3d8838c4b9f4c19
Ernesto Rodriguez <ernestorodriguez374@gmail.com>
2025-06-10 13:36:22 +0800

    000000/ https://traxcorp.atlassian.net/browse/INTERFACE-0000 Update SQL

--- INTERFACE SCRIPT.sql
+++ INTERFACE SCRIPT.sql
@@ -1,4 +1,58 @@
 -- --------------------------------------------------------
+-- Table: INTERFACE_LOCK_MASTER
+-- --------------------------------------------------------
+
+CREATE TABLE "INTERFACE_LOCK_MASTER" 
+   (	"INTERFACE_TYPE" VARCHAR2(255 CHAR), 
+	"LOCKED" NUMBER(1,0), 
+	"LOCKED_DATE" DATE, 
+	"UNLOCKED_DATE" DATE, 
+	"CURRENT_SERVER" VARCHAR2(255 CHAR), 
+	"EXEC_DELAY" NUMBER, 
+	"MAX_LOCK" NUMBER  
+   );
+
+CREATE UNIQUE INDEX "INTERFACE_LOCK_MASTER_PK" ON "INTERFACE_LOCK_MASTER" ("INTERFACE_TYPE");
+
+INSERT INTO INTERFACE_LOCK_MASTER (INTERFACE_TYPE,LOCKED,LOCKED_DATE,UNLOCKED_DATE,CURRENT_SERVER,EXEC_DELAY,MAX_LOCK) 
+VALUES ('I01',0,null,null, null,15,7200);
+
+INSERT INTO INTERFACE_LOCK_MASTER (INTERFACE_TYPE,LOCKED,LOCKED_DATE,UNLOCKED_DATE,CURRENT_SERVER,EXEC_DELAY,MAX_LOCK) 
+VALUES ('I02',0,null,null, null,15,7200);
+
+INSERT INTO INTERFACE_LOCK_MASTER (INTERFACE_TYPE,LOCKED,LOCKED_DATE,UNLOCKED_DATE,CURRENT_SERVER,EXEC_DELAY,MAX_LOCK) 
+VALUES ('I03',0,null,null, null,15,7200);
+
+INSERT INTO INTERFACE_LOCK_MASTER (INTERFACE_TYPE,LOCKED,LOCKED_DATE,UNLOCKED_DATE,CURRENT_SERVER,EXEC_DELAY,MAX_LOCK) 
+VALUES ('I31',0,null,null, null,15,600);
+
+-- --------------------------------------------------------
+-- Table: OPS_LINE_EMAIL_MASTER
+-- --------------------------------------------------------
+
+CREATE TABLE ops_line_email_master (
+    ops_line    VARCHAR2(10 CHAR),
+    email       VARCHAR2(120 CHAR),
+    site        VARCHAR2(10 BYTE),
+    cost_centre VARCHAR2(10 BYTE)
+)
+SEGMENT CREATION IMMEDIATE
+PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 NOCOMPRESS LOGGING;
+
+CREATE UNIQUE INDEX ops_line_email_master_pk ON
+    ops_line_email_master (
+        ops_line,
+        email
+    )
+        PCTFREE 10 INITRANS 2 MAXTRANS 255 COMPUTE STATISTICS;
+
+ALTER TABLE ops_line_email_master
+    ADD CONSTRAINT ops_line_email_master_pk PRIMARY KEY ( ops_line,
+                                                          email )
+        USING INDEX ops_line_email_master_pk
+    ENABLE;
+
+-- --------------------------------------------------------
 -- Table: EMPLOYEE_SCHEDULE
 -- --------------------------------------------------------
 
